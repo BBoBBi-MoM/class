856f310798b440dda6c81ca482b1ea49b28ae0dd
BBoBBi-MoM <shanghai110930@gmail.com>
2023-06-16 14:06:40 +0900

    .

--- project/11.ML/pyflask/tmp.py
+++
@@ -1,9 +0,0 @@
-#%%
-from flask import Flask,request
-from flask import render_template
-from sklearn.ensemble import GradientBoostingClassifier
-import pickle
-import joblib
-
-model = joblib.load('./best_model.pkl')
-# %%
